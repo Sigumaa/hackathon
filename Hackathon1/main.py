aa89ec34379c6f80ea66494e44d9e35729351a32
sigumaa <siguma95@gmail.com>
2021-03-12 23:47:59 +0900

    good

--- Hackathon1/main.py
+++ Hackathon1/main.py
@@ -1,14 +1,47 @@
 import discord
+from discord.ext import tasks
 
 f = open("token.txt")
-token = f.read()
+TOKEN = f.read()
 f.close()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+
+client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
-    print("logged in as " + client.user.name)
+	print("logged in\n")
+	await client.change_presence(activity=discord.Game(name="Discord", type=1))
+	loop.start()
+
+GUILD_ID = 815946801755586570
+
+id = [688227388907323472]
+
+@tasks.loop(seconds=15)
+async def loop():
+    guild = client.get_guild(GUILD_ID)
+    for member in guild.members:
+        if "online" in member.status:
+            if member.id in id:
+                print("on-line")
+        else:
+            if member.id in id:
+                print("off-line")
 
 
-client.run(token)
+@client.event
+async def on_message(message):
+
+	if message.author.bot:
+		return
+
+	if message.content == "!ping":
+		await message.channel.send("pong!")
+
+
+client.run(TOKEN)
