52cec4b48e47e1545a297d4f8e6e20e610f9b6e9
sigumaa <siguma95@gmail.com>
2021-03-13 00:32:02 +0900

    done

--- Hackathon1/main.py
+++ Hackathon1/main.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import tasks
+from datetime import datetime
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 f = open("token.txt")
 TOKEN = f.read()
@@ -22,16 +26,25 @@ GUILD_ID = 815946801755586570
 
 id = [688227388907323472]
 
-@tasks.loop(seconds=15)
+x=[]
+y=[]
+
+@tasks.loop(seconds=5)
 async def loop():
+    n = datetime.now()
     guild = client.get_guild(GUILD_ID)
     for member in guild.members:
         if "online" in member.status:
             if member.id in id:
-                print("on-line")
+                y.append(1)
+                x.append(n)
         else:
             if member.id in id:
-                print("off-line")
+                y.append(0)
+                x.append(n)
+    if n.hour == 0 and n.minute == 31:
+        plt.plot(x, y);
+        plt.savefig("a.png")
 
 
 @client.event
